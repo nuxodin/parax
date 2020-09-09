5a18068665a12f929a226f0140bd900f800c155b
Tobias Buschor <tobias.buschor@shwups.ch>
2020-09-09 16:20:25 +0200

    initial

---
+++ parax-window.js
@@ -0,0 +1,15 @@
+// lib
+
+
+
+
+wickedElements.define(
+    '[parax-window]', {
+        init() {
+        },
+        //connected() {}, // todo
+        //disconnected() {}, // todo
+        //observedAttributes: ['parax'], // todo
+        //attributeChanged(name, oldValue, newValue) {}, // todo
+    }
+);
